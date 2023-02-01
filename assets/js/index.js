2223262ebc79d928aebb12ba06edde1182e99e07
josieade3 <josieadebajo5@gmail.com>
2023-02-01 17:00:26 +0000

    removed footer and added more css

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const setDecision = (decision) => {
+    document.querySelector(".decision h1").innerText = decision;
+  }
+  
+  const setScore = (newScore) => {
+    SCORE = newScore;
+    document.querySelector(".score h1").innerText = newScore;
+  }
