0e8e7b6d563e24599177e362aa55791a237ce1ff
josieade3 <josieadebajo5@gmail.com>
2023-02-06 12:11:26 +0000

    Added DOM elements

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,18 @@
-const setDecision = (decision) => {
-    document.querySelector(".decision h1").innerText = decision;
-  }
-  
-  const setScore = (newScore) => {
-    SCORE = newScore;
-    document.querySelector(".score h1").innerText = newScore;
-  }
+/**DOM elements */
+const buttons = document.getElementsByClassName("control");
+const Playerscore = document.getElementById("player-score");
+const Computerscore = document.getElementById("computer-score");
+const PlayerImage = document.getElementById("player-image");
+const ComputerImage = document.getElementById("computer-image");
+const message = document.getElementById("messages");
+const choices = ["rock", "paper", "scissors"];
+
+/**buttons*/
+for (let button of buttons) {
+  button.addEventListener("click", function () {
+    letPlayerChoice= this.getAttribute (data-choice);
+    playGame(playerchoice);
+  });
+}
+
+
